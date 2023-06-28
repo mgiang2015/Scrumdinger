9dc46a6e6897698f9894fb345d8c67d01fa0dc3a
Le Minh Giang <mgiang2015@gmail.com>
2023-06-28 14:03:28 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Le Minh Giang on 28/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
